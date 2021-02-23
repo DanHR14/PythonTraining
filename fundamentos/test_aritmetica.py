32ad24203f08698545388ce1698b3e2084f9f5cb
daniel.hernandezr <daniel.hernandezr@col.vueling.com>
2021-02-23 20:01:28 +0100

    modules in python

---
+++ fundamentos/test_aritmetica.py
@@ -0,0 +1,7 @@
+# from modulos import modulo_aritmetica as aritmetica
+import modulos.modulo_aritmetica as aritmetica
+resultado = aritmetica.sumar(1,2)
+print(resultado)
+
+resultado = aritmetica.resta(3,2)
+print(resultado)
---
+++ fundamentos/test_persona.py
@@ -0,0 +1,4 @@
+from modulos.modulo_persona import Persona
+
+p1 = Persona("Juan", 30)
+print(p1)
