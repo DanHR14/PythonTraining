4ef74b02a8a84873fb8fa148c34e17284d122daf
daniel.hernandezr <daniel.hernandezr@col.vueling.com>
2021-03-02 12:15:33 +0100

    Templates and MVT on django

--- Django/personas_django/sap/sap/urls.py
+++ Django/personas_django/sap/sap/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import bienvenido, despedida, contacto
+from webapp.views import bienvenido
+from personas.views import detallePersona, nuevaPersona, editarPersona, eliminarPersona
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('bienvenido/', bienvenido)
-    path('', bienvenido),
-    path('despedida.html', despedida),
-    path('contacto', contacto)
+    path('', bienvenido, name='inicio'),
+    path('detalle_persona/<int:id>', detallePersona),
+    path('nueva_persona', nuevaPersona),
+    path('editar_persona/<int:id>', editarPersona),
+    path('eliminar_persona/<int:id>', eliminarPersona),
+
+
 ]
--- Django/personas_django/sap/webapp/views.py
+++ Django/personas_django/sap/webapp/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from personas.models import Persona, Domicilio
+
+
 def bienvenido(request):
-    return HttpResponse('Hola mundo desde Django')
+    num_personas = Persona.objects.count()
+    personas = Persona.objects.all()
+    personas = Persona.objects.order_by('id')
+    return render(request, 'bienvenido.html', {'num_personas': num_personas, 'personas': personas})
 
-def despedida(request):
-    return HttpResponse('Despedida desde Django')
-
-def contacto(request):
-    return HttpResponse('Dàniel, 6654787565')
